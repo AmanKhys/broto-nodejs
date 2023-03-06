e4001af75469a03dde6de386e0056760f9fe2ba9
AmanKhys <amankhys2112@gmail.com>
2023-03-06 12:02:25 +0530

    update rest of the changes from before

--- 8-express-generator/app.js
+++ 8-express-generator/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRender = require('./routes/about');
+var signupRender = require('./routes/signup');
 
 var app = express();
 
@@ -19,10 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRender);
+app.use('/signup', signupRender );
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
